1aa0b2ed434bd4d432850fe35106116903f0b1d3
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-11-15 01:14:36 -0300

    second commit

--- Semana1/dotnet_p001/Program.cs
+++ Semana1/dotnet_p001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region 2 Tipos de dados
+byte tipoByte; 
+sbyte tipoSByte; 
+short tipoShort ;
+ushort tipoUShort; 
+int tipoInt; 
+uint tipoUInt; 
+long tipoLong; 
+ulong tipoULong;
+
+tipoByte = byte.MaxValue;
+tipoSByte = sbyte.MaxValue;
+tipoShort =short.MaxValue;
+tipoUShort = ushort.MaxValue;
+tipoInt = int.MaxValue;
+tipoUInt = uint.MaxValue;
+tipoLong = long.MaxValue;
+tipoULong = ulong.MaxValue;
+
+Console.WriteLine(" Valor maximo de um byte:  " + tipoByte);
+Console.WriteLine(" Valor maximo de um sbyte:  " + tipoSByte);
+Console.WriteLine(" Valor maximo de um short:  " + tipoShort);
+Console.WriteLine(" Valor maximo de um ushort:  " + tipoUShort);
+Console.WriteLine(" Valor maximo de um int:  " + tipoInt);
+Console.WriteLine(" Valor maximo de um uint:  " + tipoUInt);
+Console.WriteLine(" Valor maximo de um long:  " + tipoLong);
+Console.WriteLine(" Valor maximo de um ulong:  " + tipoULong);
+#endregion
