ea59ddc4903901862ea4dd8e63f023b8a442aade
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-11-27 19:46:33 -0300

    Criando tupla de produtos

---
+++ Semana3/dotnet_p003/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Produto {
+    public int Codigo { get; set; }
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public decimal PrecoUnitario { get; set; }
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        Produto produto = new Produto()
+        {
+            Codigo = 123456,
+            Nome = "Caneta",
+            Quantidade = 100,
+            PrecoUnitario = 10,
+        };
+
+        Console.WriteLine("Código: " + produto.Codigo);
+        Console.WriteLine("Nome: " + produto.Nome);
+        Console.WriteLine("Quantidade: " + produto.Quantidade);
+        Console.WriteLine("Preço unitário: " + produto.PrecoUnitario);
+    }
+}
