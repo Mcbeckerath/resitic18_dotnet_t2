8d46fa4cea17847a6eb2cdf489be7a96d67c8e3f
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-11-28 17:08:07 -0300

    Subindo classe academia

--- prova_individual/Program.cs
+++ prova_individual/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Pessoa
 {
@@ -85,4 +86,27 @@ public class Cliente : Pessoa
         Altura = altura;
         Peso = peso;
     }
+}
+public class Academia
+{
+    public List<Cliente> Clientes { get; private set; }
+    public List<Treinador> Treinadores { get; private set; }
+
+    public Academia()
+    {
+        Clientes = new List<Cliente>();
+        Treinadores = new List<Treinador>();
+    }
+
+    public void AdicionarCliente(Cliente cliente)
+    {
+        Clientes.Add(cliente);
+    }
+
+    public void AdicionarTreinador(Treinador treinador)
+    {
+        Treinadores.Add(treinador);
+    }
+
+
 }
