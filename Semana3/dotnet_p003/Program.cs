463d9daef4a1fc2764a894f5437979f89e095909
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-11-27 23:02:17 -0300

    subindo consulta de produtos

--- Semana3/dotnet_p003/Program.cs
+++ Semana3/dotnet_p003/Program.cs
@@ -13,67 +13,134 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // Criando uma lista para armazenar produtos
         List<Produto> estoque = new List<Produto>();
 
-        // Cadastro de produtos
         do
         {
-            try
+            Console.WriteLine("\n1 - Cadastro de Produto");
+            Console.WriteLine("2 - Consulta de Produto");
+            Console.WriteLine("3 - Listar Estoque");
+            Console.WriteLine("4 - Sair");
+            Console.Write("Escolha uma opção: ");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
             {
-                Console.WriteLine("\nCadastro de Produto:");
+                case "1":
+                    CadastrarProduto(estoque);
+                    break;
 
-                // Solicitando informações do produto ao usuário
-                Console.Write("Código: ");
-                int codigo = int.Parse(Console.ReadLine());
+                case "2":
+                    ConsultarProduto(estoque);
+                    break;
 
-                Console.Write("Nome: ");
-                string nome = Console.ReadLine();
+                case "3":
+                    ListarEstoque(estoque);
+                    break;
 
-                Console.Write("Quantidade: ");
-                int quantidade = int.Parse(Console.ReadLine());
+                case "4":
+                    Console.WriteLine("Saindo do programa.");
+                    return;
 
-                Console.Write("Preço Unitário: ");
-                decimal precoUnitario = decimal.Parse(Console.ReadLine());
-
-                // Criando o objeto Produto e adicionando à lista
-                Produto novoProduto = new Produto
-                {
-                    Codigo = codigo,
-                    Nome = nome,
-                    Quantidade = quantidade,
-                    PrecoUnitario = precoUnitario
-                };
-
-                estoque.Add(novoProduto);
-
-                Console.WriteLine("Produto cadastrado com sucesso!");
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
             }
-            catch (FormatException)
-            {
-                Console.WriteLine("Erro: Formato inválido. Certifique-se de inserir um valor numérico para Código, Quantidade e Preço.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Erro: {ex.Message}");
-            }
-
-            // Perguntando se o usuário deseja cadastrar outro produto
-            Console.Write("Deseja cadastrar outro produto? (S/N): ");
-        } while (Console.ReadLine().ToUpper() == "S");
-
-        // Exibindo o estoque
-        Console.WriteLine("\nEstoque Atual:");
-        ListarEstoque(estoque);
+        } while (true);
     }
 
-    // Função para listar o estoque
+    static void CadastrarProduto(List<Produto> estoque)
+    {
+        try
+        {
+            Console.WriteLine("\nCadastro de Produto:");
+
+            Console.Write("Código: ");
+            int codigo = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Preço Unitário: ");
+            decimal precoUnitario = decimal.Parse(Console.ReadLine());
+
+            Produto novoProduto = new Produto
+            {
+                Codigo = codigo,
+                Nome = nome,
+                Quantidade = quantidade,
+                PrecoUnitario = precoUnitario
+            };
+
+            estoque.Add(novoProduto);
+
+            Console.WriteLine("Produto cadastrado com sucesso!");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Formato inválido. Certifique-se de inserir um valor numérico para Código, Quantidade e Preço.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+
+    static void ConsultarProduto(List<Produto> estoque)
+    {
+        try
+        {
+            Console.Write("\nConsulta de Produto - Digite o Código: ");
+            int codigoConsulta = int.Parse(Console.ReadLine());
+
+            Produto produtoConsultado = BuscarProdutoPorCodigo(estoque, codigoConsulta);
+
+            Console.WriteLine($"Produto encontrado - Código: {produtoConsultado.Codigo}, Nome: {produtoConsultado.Nome}, Quantidade: {produtoConsultado.Quantidade}, Preço Unitário: {produtoConsultado.PrecoUnitario:C}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Formato inválido. Certifique-se de inserir um valor numérico para o Código.");
+        }
+        catch (ProdutoNaoEncontradoException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+
     static void ListarEstoque(List<Produto> estoque)
     {
+        Console.WriteLine("\nEstoque Atual:");
         foreach (var produto in estoque)
         {
             Console.WriteLine($"Código: {produto.Codigo}, Nome: {produto.Nome}, Quantidade: {produto.Quantidade}, Preço Unitário: {produto.PrecoUnitario:C}");
         }
+    }
+
+    static Produto BuscarProdutoPorCodigo(List<Produto> estoque, int codigo)
+    {
+        Produto produtoEncontrado = estoque.Find(produto => produto.Codigo == codigo);
+
+        if (produtoEncontrado == null)
+        {
+            throw new ProdutoNaoEncontradoException($"Produto com código {codigo} não encontrado.");
+        }
+
+        return produtoEncontrado;
+    }
+}
+
+public class ProdutoNaoEncontradoException : Exception
+{
+    public ProdutoNaoEncontradoException(string mensagem) : base(mensagem)
+    {
     }
 }
 
