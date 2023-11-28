e6144299fae8d4ff48dbaf2fac8582b2ebdcfce8
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-11-28 16:27:31 -0300

    Subindo classe pessoa

---
+++ prova_individual/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Pessoa
+{
+    private string cpf;
+
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+
+    public string CPF
+    {
+        get { return cpf; }
+        set
+        {
+            if (value.Length != 11)
+            {
+                throw new ArgumentException("O CPF deve ter exatamente 11 dígitos.");
+            }
+            cpf = value;
+        }
+    }
+
+    public Pessoa(string nome, DateTime dataNascimento, string cpf)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
+    }
+}
+
+
